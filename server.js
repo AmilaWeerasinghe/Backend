26fa78a29409c60cf89fcc9965d6a6f24b8cfe13
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2023-07-21 15:36:00 +0530

    updated_basic_node_server

--- server.js
+++ server.js
@@ -4,16 +4,35 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5000; // You can change this port number to your preference
+const port = 5001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Define your API endpoints here
-app.get('/api/data', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+// Sample data
+let items = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+];
+
+// API endpoints
+app.get('/api/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/api/items', (req, res) => {
+  const newItem = { id: items.length + 1, name: req.body.name };
+  items.push(newItem);
+  res.status(201).json(newItem);
+});
+
+app.delete('/api/items/:id', (req, res) => {
+  const itemId = parseInt(req.params.id);
+  items = items.filter((item) => item.id !== itemId);
+  res.sendStatus(204);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
